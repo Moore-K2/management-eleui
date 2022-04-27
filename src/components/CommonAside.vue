02fe7bf479518d2dacd0e41b309caf44eb98d7ee
Moore-K2 <1104326586@qq.com>
2022-04-27 22:17:51 +0800

    二级菜单

--- src/components/CommonAside.vue
+++ src/components/CommonAside.vue
@@ -17,14 +17,24 @@
         <span slot="title">{{ item.label }}</span>
       </el-menu-item>
 
-      <el-submenu index="1">
+      <!-- 一级菜单 -->
+      <el-submenu
+        v-for="item in hasChildren"
+        :key="item.path"
+        :index="item.path + ''"
+      >
         <template slot="title">
-          <i class="el-icon-location"></i>
-          <span slot="title">导航一</span>
+          <i :class="'el-icon-' + item.icon"></i>
+          <span slot="title">{{ item.label }}</span>
         </template>
-        <el-menu-item-group>
-          <span slot="title">分组一</span>
-          <el-menu-item index="1-1">选项1</el-menu-item>
+        <!-- 二级菜单 -->
+        <el-menu-item-group
+          v-for="(subItem, subIndex) in item.children"
+          :key="subItem.path"
+        >
+          <el-menu-item :index="subIndex">
+            {{ subItem.label }}
+          </el-menu-item>
         </el-menu-item-group>
       </el-submenu>
     </el-menu>
@@ -68,7 +78,7 @@ export default {
             {
               path: "/page1",
               name: "page1",
-              label: "页面",
+              label: "页面1",
               icon: "setting",
               url: "Other/PageOne",
             },
